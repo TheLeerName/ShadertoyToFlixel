fbf829d4c79c3b5e4776e881dd44a4502a8d932e
TheLeerName <lesha.nechaev.05@mail.ru>
2023-05-25 02:29:32 +0700

    changed almost everything 😘

--- assets/ShadertoyToFlixel.js
+++ assets/ShadertoyToFlixel.js
@@ -1,4 +1,3 @@
-const { trace } = require('console')
 const fs = require('fs')
 const dialog = require('node-file-dialog').dialog
 
@@ -21,25 +20,19 @@ function doThing(file) {
 	//var file = fs.readFileSync(process.argv[2]).toString().split('\n')
 	var output = file.substring(0, file.lastIndexOf("/") + 1) + 'flixel-' + file.substring(file.lastIndexOf("/") + 1)
 	file = fs.readFileSync(file).toString().split("\n")
-	var bbpanzuFix = [
-		"// Automatically converted with ShadertoyToFlixel.js",
-		"#pragma header",
-		"",
-		"vec2 uv = openfl_TextureCoordv.xy;",
-		"vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;",
-		"vec2 iResolution = openfl_TextureSize;",
-		"uniform float iTime;",
-		"#define iChannel0 bitmap",
-		"#define texture flixel_texture2D",
-		"#define fragColor gl_FragColor",
-		"#define mainImage main",
-		""
-	]
-	console.log("[TRACE] Adding flixel defines...")
-	file = bbpanzuFix.concat(file)
+
+	file.splice(0, 0, "// Automatically converted with ShadertoyToFlixel.js")
+	file.splice(1, 0, "")
+
+	var pragmaHeader = false
+	var iTime = false
+	var uv = false
+	var fragCoord = false
+	var iResolution = false
 
 	var fixedAlpha = false
 	var fixedMain = false
+
 	for (let i = 0; i < file.length; i++) {
 		if (file[i].includes('void mainImage(')) {
 			console.log("[TRACE] Fixing void mainImage...")
@@ -51,7 +44,55 @@ function doThing(file) {
 			file[i] = file[i].substring(0, file[i].indexOf(",") + 1) + " uv);"
 			fixedAlpha = true
 		}
+		if (!file[i].includes("#define")) {
+			if (file[i].includes("iChannel0")) {
+				file[i] = file[i].replaceAll("iChannel0", "bitmap");
+				console.log("[TRACE] Replaced iChannel0 to bitmap in line " + i + "!")
+			}
+			if (file[i].includes("texture")) {
+				file[i] = file[i].replaceAll("texture", "flixel_texture2D");
+				console.log("[TRACE] Replaced texture to flixel_texture2D in line " + i + "!")
+
+			}
+			if (file[i].includes("fragColor")) {
+				file[i] = file[i].replaceAll("fragColor", "gl_FragColor");
+				console.log("[TRACE] Replaced fragColor to gl_FragColor in line " + i + "!")
+
+			}
+			if (file[i].includes("mainImage")) {
+				file[i] = file[i].replaceAll("mainImage", "main");
+				console.log("[TRACE] Replaced mainImage to main in line " + i + "!")
+
+			}
+		}
+		if (file[i].includes("#pragma header")) pragmaHeader = true;
+		if (file[i].includes("vec2 uv = ")) uv = true;
+		if (file[i].includes("vec2 fragCoord = ")) fragCoord = true;
+		if (file[i].includes("vec2 iResolution = ")) iResolution = true;
+		if (file[i].includes("uniform float iTime;")) iTime = true;
 	}
+	if (!pragmaHeader) {
+		file.splice(2, 0, "#pragma header")
+		console.log("[TRACE] Added #pragma header!")
+	}
+	if (!uv) {
+		file.splice(3, 0, "vec2 uv = openfl_TextureCoordv.xy;")
+		console.log("[TRACE] Added vec2 uv!")
+	}
+	if (!fragCoord) {
+		file.splice(4, 0, "vec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;")
+		console.log("[TRACE] Added vec2 fragCoord!")
+	}
+	if (!iResolution) {
+		file.splice(5, 0, "vec2 iResolution = openfl_TextureSize;")
+		console.log("[TRACE] Added vec2 iResolution!")
+	}
+	if (!iTime) {
+		file.splice(6, 0, "uniform float iTime;")
+		console.log("[TRACE] Added uniform float iTime!")
+	}
+	file.splice(7, 0, "")
+
 	if (!fixedMain) console.log("[ERROR] Can't fix void mainImage!")
 	if (!fixedAlpha) console.log("[ERROR] Can't fix alpha channel!")
 
--- run.bat
+++ run.bat
@@ -1,7 +1,5 @@
 @echo off
 @WHERE node >nul 2>nul
 @IF %ERRORLEVEL% NEQ 0 echo [ERROR] You don't have node.js installed! Visit: https://nodejs.org/
-@IF %ERRORLEVEL% NEQ 1 cd assets
-@IF %ERRORLEVEL% NEQ 1 call dependencies.bat >nul 2>nul
-@IF %ERRORLEVEL% NEQ 1 node ShadertoyToFlixel.js
+@IF %ERRORLEVEL% NEQ 1 node assets/ShadertoyToFlixel.js
 pause
