6891c6d5f8196b4da751104f59682576fffd2a9d
TheLeerName <lesha.nechaev.05@mail.ru>
2023-05-25 20:45:22 +0700

    fixed gl_FragColor

--- assets/ShadertoyToFlixel.js
+++ assets/ShadertoyToFlixel.js
@@ -34,16 +34,6 @@ function doThing(file) {
 	var fixedMain = false
 
 	for (let i = 0; i < file.length; i++) {
-		if (file[i].includes('void mainImage(')) {
-			console.log("[TRACE] Fixing void mainImage...")
-			file[i] = 'void mainImage()'
-			fixedMain = true
-		}
-		if (file[i].includes('fragColor = vec4(')) {
-			console.log("[TRACE] Fixing alpha channel...")
-			file[i] = file[i].substring(0, file[i].indexOf(",") + 1) + " uv);"
-			fixedAlpha = true
-		}
 		if (!file[i].includes("#define")) {
 			if (file[i].includes("iChannel0")) {
 				file[i] = file[i].replaceAll("iChannel0", "bitmap");
@@ -52,17 +42,14 @@ function doThing(file) {
 			if (file[i].includes("texture")) {
 				file[i] = file[i].replaceAll("texture", "flixel_texture2D");
 				console.log("[TRACE] Replaced texture to flixel_texture2D in line " + i + "!")
-
 			}
 			if (file[i].includes("fragColor")) {
 				file[i] = file[i].replaceAll("fragColor", "gl_FragColor");
 				console.log("[TRACE] Replaced fragColor to gl_FragColor in line " + i + "!")
-
 			}
 			if (file[i].includes("mainImage")) {
 				file[i] = file[i].replaceAll("mainImage", "main");
 				console.log("[TRACE] Replaced mainImage to main in line " + i + "!")
-
 			}
 		}
 		if (file[i].includes("#pragma header")) pragmaHeader = true;
@@ -70,6 +57,17 @@ function doThing(file) {
 		if (file[i].includes("vec2 fragCoord = ")) fragCoord = true;
 		if (file[i].includes("vec2 iResolution = ")) iResolution = true;
 		if (file[i].includes("uniform float iTime;")) iTime = true;
+		
+		if (file[i].includes('void main(')) {
+			console.log("[TRACE] Fixing void main...")
+			file[i] = 'void main()'
+			fixedMain = true
+		}
+		if (file[i].includes('gl_FragColor = vec4(')) {
+			console.log("[TRACE] Fixing alpha channel...")
+			file[i] = file[i].substring(0, file[i].indexOf(",") + 1) + " uv);"
+			fixedAlpha = true
+		}
 	}
 	if (!pragmaHeader) {
 		file.splice(2, 0, "#pragma header")
