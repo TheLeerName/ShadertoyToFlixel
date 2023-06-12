faee05013f390dbe44c1d6236305d79963b9cf0a
TheLeerName <lesha.nechaev.05@mail.ru>
2023-06-12 23:06:41 +0700

    symbols now doesnt care to fixing alpha channel

--- assets/ShadertoyToFlixel.js
+++ assets/ShadertoyToFlixel.js
@@ -12,30 +12,31 @@ doThing = (file) => {
 	var fixedMain = false
 
 	for (let i = 0; i < file.length; i++) {
-		if (!file[i].includes("#define")) {
-			if (file[i].includes("mainImage")) {
+		var tocheck = file[i].trim().replaceAll(" ", "").replaceAll("\t", "").replaceAll("\r", "")
+		if (!tocheck.includes("#define")) {
+			if (tocheck.includes("mainImage")) {
 				file[i] = file[i].replaceAll("mainImage", "main")
 				console.log("[TRACE] Replaced mainImage to main in line " + i + "!")
 			}
-			if (file[i].includes("iChannel0")) {
+			if (tocheck.includes("iChannel0")) {
 				file[i] = file[i].replaceAll("iChannel0", "bitmap")
 				console.log("[TRACE] Replaced iChannel0 to bitmap in line " + i + "!")
 			}
-			if (file[i].includes("texture(")) {
+			if (tocheck.includes("texture(")) {
 				file[i] = file[i].replaceAll("flixel_texture2D(", "texture(")
 				file[i] = file[i].replaceAll("texture(", "flixel_texture2D(")
 				console.log("[TRACE] Replaced texture to flixel_texture2D in line " + i + "!")
 			}
-			if (file[i].includes("fragColor") && !file[i].includes('void main(')) {
+			if (tocheck.includes("fragColor") && !tocheck.includes('void main(')) {
 				file[i] = file[i].replaceAll("fragColor", "gl_FragColor")
 				console.log("[TRACE] Replaced fragColor to gl_FragColor in line " + i + "!")
 			}
 		}
 		if (file[i].includes("// Automatically converted with ShadertoyToFlixel.js")) watermark = true
 		if (file[i].includes("#pragma header")) pragmaHeader = true
-		if (file[i].includes("vec2 uv = ")) uv = true
-		if (file[i].includes("vec2 fragCoord = ")) fragCoord = true
-		if (file[i].includes("vec2 iResolution = ")) iResolution = true
+		if (tocheck.includes("vec2uv=")) uv = true
+		if (tocheck.includes("vec2fragCoord=")) fragCoord = true
+		if (tocheck.includes("vec2iResolution=")) iResolution = true
 		if (file[i].includes("uniform float iTime;")) iTime = true
 		
 		if (file[i].includes('void main(')) {
@@ -45,11 +46,30 @@ doThing = (file) => {
 			console.log("[TRACE] Fixed void main!")
 			fixedMain = true
 		}
-		if (file[i].includes('gl_FragColor = vec4(') && file[i].includes(',')) {
-			file[i] = file[i].substring(0, file[i].lastIndexOf(",") + 1) + " flixel_texture2D(bitmap, uv).a);"
-			console.log("[TRACE] Fixed alpha channel!")
-			fixedAlpha = true
-		}
+		if (tocheck.includes('gl_FragColor=vec4('))
+			file = fixAlphaChannel(file, i);
+	}
+
+	function fixAlphaChannel(file, i) {
+		// doing vars
+		var str = file.join('\n')
+		var alphaStr = str
+		alphaStr = alphaStr.substring(alphaStr.lastIndexOf(file[i]))
+		alphaStr = alphaStr.substring(0, alphaStr.lastIndexOf(");"))
+
+		// finding and replacing alpha value
+		var alphaValue = alphaStr.substring(alphaStr.lastIndexOf(',') + 1).trim().replaceAll(" ", "").replaceAll("\t", "").replaceAll("\r", "")
+		alphaStr = alphaStr.substring(0, alphaStr.lastIndexOf(',')) + alphaStr.substring(alphaStr.lastIndexOf(',')).replaceAll(alphaValue, "flixel_texture2D(bitmap, uv).a")
+
+		// adding new alpha value to shader
+		var prefix = str.substring(0, str.lastIndexOf(file[i]))
+		var suffix = str.substring(str.lastIndexOf(file[i])).substring(str.substring(str.lastIndexOf(file[i])).lastIndexOf(");"))
+		str = prefix + alphaStr + suffix
+
+		console.log("[TRACE] Fixed alpha channel!")
+		fixedAlpha = true
+
+		return str.split('\n')
 	}
 
 	var whatever = []
