3ccdbcd4d0491d62a5e2a821fab109093c0f0de0
TheLeerName <lesha.nechaev.05@mail.ru>
2023-06-27 04:10:42 +0700

    bunch of fixes and additions
    - fix of custom named fragColor and fragCoord
    - fully changed how void mainImage() will fix
    - added iChannel0...3
    - iChannelResolution[0] replaces with iResolution

--- assets/ShadertoyToFlixel.js
+++ assets/ShadertoyToFlixel.js
@@ -3,25 +3,21 @@ doThing = (file) => {
 
 	var watermark = false
 	var pragmaHeader = false
-	var iTime = false
-	var fragCoord = false
+
+	// shadertoy variables (not all for now!!!)
 	var iResolution = false
+	var iTime = false
+	var iChannel0 = false
+	var iChannel1 = false
+	var iChannel2 = false
+	var iChannel3 = false
+
+	var main = false
 
 	var fixedAlpha = false
-	var fixedMain = false
-	
-	var voidMainLine = 0
 
 	for (let i = 0; i < file.length; i++) {
 		var tocheck = file[i].trim().replaceAll(" ", "").replaceAll("\t", "").replaceAll("\r", "") // `vec2 uv ;` => `vec2uv;`
-		if (tocheck.includes("mainImage")) {
-			file[i] = file[i].replaceAll("mainImage", "main")
-			console.log("[TRACE] Replaced mainImage to main in line " + i + "!")
-		}
-		if (tocheck.includes("iChannel0")) {
-			file[i] = file[i].replaceAll("iChannel0", "bitmap")
-			console.log("[TRACE] Replaced iChannel0 to bitmap in line " + i + "!")
-		}
 		if (tocheck.includes("texture2D(")) {
 			file[i] = file[i].replaceAll("flixel_texture2D(", "texture2D(")
 			file[i] = file[i].replaceAll("texture2D(", "flixel_texture2D(")
@@ -32,24 +28,35 @@ doThing = (file) => {
 			file[i] = file[i].replaceAll("texture(", "flixel_texture2D(")
 			console.log("[TRACE] Replaced texture to flixel_texture2D in line " + i + "!")
 		}
-		if (tocheck.includes("fragColor") && (!file[i].includes('void main(') && !file[i].includes('void mainImage('))) {
-			file[i] = file[i].replaceAll("fragColor", "gl_FragColor")
-			console.log("[TRACE] Replaced fragColor to gl_FragColor in line " + i + "!")
-		}
 		if (tocheck.includes("round(")) {
 			file[i] = file[i].replaceAll("round(", "floor(")
 			console.log("[TRACE] Replaced round to floor in line " + i + "! (floor only in #version 130 and later)")
 		}
 
-		if (file[i].includes("// Automatically converted with https://github.com/TheLeerName/ShadertoyToFlixel") || file[i].includes("// Automatically converted with ShadertoyToFlixel.js")) watermark = true
-		if (file[i].includes("#pragma header")) pragmaHeader = true
-		if (tocheck.includes("vec2fragCoord=")) fragCoord = true
-		if (tocheck.includes("vec2iResolution=")) iResolution = true
-		if (file[i].includes("uniform float iTime;")) iTime = true
-
 		if (tocheck.startsWith("#undef")) {
 			file.splice(i, 1)
 			i--
+		}
+
+		if (file[i].includes("// Automatically converted with https://github.com/TheLeerName/ShadertoyToFlixel") || file[i].includes("// Automatically converted with ShadertoyToFlixel.js")) watermark = true
+		if (file[i].includes("#pragma header")) pragmaHeader = true
+		if (file[i].includes("#define iResolution openfl_TextureSize")) iResolution = true
+		if (file[i].includes("uniform float iTime;")) iTime = true
+		if (file[i].includes("#define iChannel0 bitmap")) iChannel0 = true
+		if (file[i].includes("uniform sampler2D iChannel1;")) iChannel1 = true
+		if (file[i].includes("uniform sampler2D iChannel2;")) iChannel2 = true
+		if (file[i].includes("uniform sampler2D iChannel3;")) iChannel3 = true
+		
+		if (file[i].includes("void main()")) main = true
+
+		if (tocheck.includes("textureSize(bitmap,0)")) {
+			file[i] = file[i].replaceAll("textureSize(bitmap, 0)", "iResolution.xy")
+			file[i] = file[i].replaceAll("textureSize(bitmap,0)", "iResolution.xy")
+			console.log("[TRACE] Replaced textureSize(bitmap, 0) to iResolution.xy in line " + i + "!  (textureSize only in #version 130 and later)")
+		}
+		if (tocheck.includes("iChannelResolution[0]")) {
+			file[i] = file[i].replaceAll("iChannelResolution[0]", "iResolution")
+			console.log("[TRACE] Replaced iChannelResolution[0] to iResolution in line " + i + "!")
 		}
 	}
 
@@ -57,27 +64,6 @@ doThing = (file) => {
 		var tocheck = file[i].trim().replaceAll(" ", "").replaceAll("\t", "").replaceAll("\r", "")
 		if (tocheck.includes('gl_FragColor=vec4('))
 			file = fixAlphaChannel(file, i)
-		if (file[i].includes('void main('))
-			file = fixVoidMain(file, i)
-	}
-
-	function fixVoidMain(file, i) {
-		var bracket = file[i].includes('{')
-		voidMainLine = i + 0
-		if (!bracket) voidMainLine++
-
-		if (!file[i].includes("out vec4 fragColor")) {
-			fixedMain = true
-			return file
-		}
-
-		file[i] = 'void main()'
-		if (bracket) file[i] = file[i] + ' {'
-
-		console.log("[TRACE] Fixed void main! (flixel not uses args in void main)")
-		fixedMain = true
-
-		return file
 	}
 
 	function fixAlphaChannel(file, i) {
@@ -119,39 +105,59 @@ doThing = (file) => {
 	}
 	var whatever = []
 
+	// adds things in start of file
 	if (!watermark) {
 		whatever.push("// Automatically converted with https://github.com/TheLeerName/ShadertoyToFlixel")
-		whatever.push("")
 	}
+
+	if (whatever.length > 0) whatever.push("")
+
 	if (!pragmaHeader) {
 		whatever.push("#pragma header")
-		whatever.push("")
 		console.log("[TRACE] Added #pragma header!")
+	}
+
+	if (whatever.length > 0) whatever.push("")
+
+	if (!iResolution) {
+		whatever.push("#define iResolution openfl_TextureSize")
+		console.log("[TRACE] Added iResolution!")
 	}
 	if (!iTime) {
 		whatever.push("uniform float iTime;")
-		console.log("[TRACE] Added uniform float iTime!")
+		console.log("[TRACE] Added iTime!")
 	}
-
-	if (fixedMain) {
-		var oldLength = file.length
-		if (!fragCoord) {
-			file.splice(voidMainLine + 1, 0, "\tvec2 fragCoord = openfl_TextureCoordv*openfl_TextureSize;")
-			console.log("[TRACE] Added vec2 fragCoord!")
-		}
-		if (!iResolution) {
-			file.splice(voidMainLine + 1, 0, "\tvec2 iResolution = openfl_TextureSize;")
-			console.log("[TRACE] Added vec2 iResolution!")
-		}
-		if (oldLength != file.length) file.splice(voidMainLine + 1 + (file.length - oldLength), 0, "")
+	if (!iChannel0) {
+		whatever.push("#define iChannel0 bitmap")
+		console.log("[TRACE] Added iChannel0!")
+	}
+	if (!iChannel1) {
+		whatever.push("uniform sampler2D iChannel1;")
+		console.log("[TRACE] Added iChannel1!")
+	}
+	if (!iChannel2) {
+		whatever.push("uniform sampler2D iChannel2;")
+		console.log("[TRACE] Added iChannel2!")
+	}
+	if (!iChannel3) {
+		whatever.push("uniform sampler2D iChannel3;")
+		console.log("[TRACE] Added iChannel3!")
 	}
 
 	if (whatever.length > 0) whatever.push("")
 
 	file = whatever.concat(file)
 
-	if (!fixedMain) console.log("[ERROR] Can't fix void main!")
-	if (!fixedAlpha) console.log("[ERROR] Can't fix alpha channel!")
+	// adds things in end of file
+	if (!main) {
+		file.push("")
+		file.push("void main() {")
+		file.push("\tmainImage(gl_FragColor, openfl_TextureCoordv*openfl_TextureSize);")
+		file.push("}")
+		console.log("[TRACE] Added void main!")
+	}
+
+	if (!fixedAlpha) console.log("[ERROR] Can't fix alpha channel! Maybe it already working properly?")
 
 	return file.join('\n')
 }
